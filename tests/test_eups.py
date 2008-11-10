1225d6dd59ad539fcca4952351705964bfd22a0c
Robert Lupton <rhl@astro.princeton.edu>
2008-11-10 19:03:28 +0000

    Made version tests more readable; changed test to reflect the fact that v1... and 1... sort as strings

--- tests/test_eups.py
+++ tests/test_eups.py
@@ -7,7 +7,7 @@ import pdb                              # we may want to say pdb.set_trace()
 import os
 import sys
 import unittest
-import neups
+import eups
 
 try:
     type(verbose)
@@ -19,7 +19,7 @@ except NameError:
 class tableTestCase(unittest.TestCase):
     """Test reading TableFiles"""
     def setUp(self):
-        self.table = neups.Table("mwi.table")
+        self.table = eups.Table("mwi.table")
 
     def tearDown(self):
         del self.table
@@ -32,7 +32,7 @@ class tableTestCase(unittest.TestCase):
 class oldTableTestCase(unittest.TestCase):
     """Test reading old-style TableFiles"""
     def setUp(self):
-        self.table = neups.Table("dervish.table")
+        self.table = eups.Table("dervish.table")
 
     def tearDown(self):
         del self.table
@@ -45,7 +45,7 @@ class oldTableTestCase(unittest.TestCase):
 class versionFileTestCase(unittest.TestCase):
     """Test reading version files"""
     def setUp(self):
-        self.version = neups.Version("fw.version")
+        self.version = eups.VersionFile("fw.version")
 
     def tearDown(self):
         del self.version
@@ -58,7 +58,7 @@ class versionFileTestCase(unittest.TestCase):
 class currentFileTestCase(unittest.TestCase):
     """Test reading current files"""
     def setUp(self):
-        self.current = neups.Current("fw.current")
+        self.current = eups.Current("fw.current")
         self.assertTrue = lambda x, y: x == y
 
     def tearDown(self):
@@ -75,7 +75,7 @@ class versionOrdering(unittest.TestCase):
     """Test eups's version ordering"""
 
     def setUp(self):
-        self.eups = neups.Eups()
+        self.eups = eups.Eups()
 
     def tearDown(self):
         del self.eups
@@ -83,57 +83,69 @@ class versionOrdering(unittest.TestCase):
     def testVersions(self):
         # return -1 if v1 < v2; 0 if v1 == v2; 1 if v1 > v2
         tests = (
-            ["aa", "aa", 0],
-            ["aa.2","aa.1", 1],
-            ["aa.2.1","aa.2", 1],
-            ["aa.2.1","aa.2.2", -1],
-            ["aa.2.1","aa.3", -1],
-            ["aa.2.b","aa.2.a", 1],
-            ["aa.2.b","aa.2.c", -1],
-            ["v1_0_0","1.0.2", -1],
-            ["1_0_0","v1.0.2", -1],
-            ["v1_0_3","a1.0.2", -1],
-            ["v1_0_0","v1.0.2", -1],
-            ["v1_0_3","v1.0.2", 1],
-            ["v1_0_3","v1_0_3", 0],
-            ["v1_0_3m1","v1_0_3", -1],
-            ["v1_0_3p1","v1_0_3", +1],
-            ["v2_0","v1_0", +1],
-            ["v2_0","v3_0", -1],
-            ["v1.2.3","v1.2.3+a", -1],
-            ["v1.2-0","v1.2.3", -1],
-            ["v1.2-4","v1.2.3", -1],
-            ["1","1-a", 1],
-            ["1","1+a", -1],
-            ["1-a","1", -1],
-            ["1-b","1-a", 1],
-            ["1+a","1+b", -1],
-            ["1-a", "1+a", -1],
-            ["1+a", "1-a", 1],
-            ["1-rc2+a", "1-rc2", 1],
-            ["1-rc2+a", "1-rc2+b", -1],
-            ["1", "1", 0 ],
-            ["1.2", "1.1", +1],
-            ["1.2.1", "1.2", +1],
-            ["1.2.1", "1.2.2", -1],
-            ["1.2.1", "1.3", -1],
-            ["1_0_2", "1.0.0", +1],
-            ["1.2-rc1", "1.2", -1],
-            ["1.2-rc1", "1.2-rc2", -1],
-            ["1.2-rc1", "1.2.3", -1],
-            ["1.2-rc4", "1.2.3", -1],
-            ["1.2+h1", "1.2", +1],
-            ["1.2-rc1+h1", "1.2-rc1", +1],
+            ["aa", "==", "aa"],
+            ["aa.2", ">", "aa.1"],
+            ["aa.2.1", ">", "aa.2"],
+            ["aa.2.1", "<", "aa.2.2"],
+            ["aa.2.1", "<", "aa.3"],
+            ["aa.2.b", ">", "aa.2.a"],
+            ["aa.2.b", "<", "aa.2.c"],
+            ["v1_0_0", ">", "1.0.2"],
+            ["1_0_0", "<", "v1.0.2"],
+            ["v1_0_3", "<", "a1.0.2"],
+            ["v1_0_0", "<", "v1.0.2"],
+            ["v1_0_3", ">", "v1.0.2"],
+            ["v1_0_3", "==", "v1_0_3"],
+            ["v1_0_3m1", "<", "v1_0_3"],
+            ["v1_0_3p1", ">", "v1_0_3"],
+            ["v2_0", ">", "v1_0"],
+            ["v2_0", "<", "v3_0"],
+            ["v1.2.3", "<", "v1.2.3+a"],
+            ["v1.2-0", "<", "v1.2.3"],
+            ["v1.2-4", "<", "v1.2.3"],
+            ["1", ">", "1-a"],
+            ["1", "<", "1+a"],
+            ["1-a", "<", "1"],
+            ["1-b", ">", "1-a"],
+            ["1+a", "<", "1+b"],
+            ["1-a", "<",  "1+a"],
+            ["1+a", ">",  "1-a"],
+            ["1-rc2+a", ">",  "1-rc2"],
+            ["1-rc2+a", "<",  "1-rc2+b"],
+            ["1", "==", "1"],
+            ["1.2", ">",  "1.1"],
+            ["1.2.1", ">",  "1.2"],
+            ["1.2.1", "<",  "1.2.2"],
+            ["1.2.1", "<",  "1.3"],
+            ["1_0_2", ">",  "1.0.0"],
+            ["1.2-rc1", "<",  "1.2"],
+            ["1.2-rc1", "<",  "1.2-rc2"],
+            ["1.2-rc1", "<",  "1.2.3"],
+            ["1.2-rc4", "<",  "1.2.3"],
+            ["1.2+h1", ">",  "1.2"],
+            ["1.2-rc1+h1", ">",  "1.2-rc1"],
             )
+
+        if False:
+            tests = (
+                ["v1_0_0", "<", "1.0.2"],
+                )
 
         nbad = 0
         for test in tests:
-            vname, v, expected = test
+            vname, expected, v = test
             result = self.eups.version_cmp(vname, v)
             #print >> sys.stderr, "version_cmp(\"%s\", \"%s\") == %s" % (vname, v, result)
+            if result == 0:
+                result = "=="
+            elif result == 1:
+                result = ">"
+            else:
+                result = "<"
+
             if result != expected:
                 nbad += 1
-                print >> sys.stderr, "%-10s %-10s: %2d (expected %2d)" % (vname, v, result, expected)
+                print >> sys.stderr, "%-10s %2s %-10s (expected %2s)" % (vname, result, v, expected)
 
         assert(nbad == 0)
 
