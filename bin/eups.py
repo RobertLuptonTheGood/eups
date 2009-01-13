13a91b9b5b818c0d8099d57ca4d6e6a23345ecea
Robert Lupton <rhl@astro.princeton.edu>
2009-01-13 16:31:52 +0000

    Rewrote the version callbacks to allow them to declare that versions don't sort.  Moved code around to allow callback to call the usual sort function, and for e.g. eups list to list unsortable versions

--- bin/eups.py
+++ bin/eups.py
@@ -72,45 +72,45 @@ except NameError:
 
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-class VersionCallbacks(object):
-    """Callback to allow users to customize behaviour by defining hooks in EUPS_STARTUP
-        and calling eups.versionCallbacks.add(hook)"""
+class VersionCallback(object):
+    """Callback to allow users to customize behaviour by defining a hook in EUPS_STARTUP
+        and calling eups.versionCallback.set(hook)"""
 
-    callbacks = []
+    callback = [None]
 
     def __init__(self):
         pass
 
-    def add(self, callback):
+    def set(self, callback):
         """
-        Add a version callback.
+        Define a version callback.
         
-        The arguments are the two version strings, and the return value is the
-        (maybe modified) versions that you prefer
+        The arguments are the two version strings and the usual comparison function, and the return value is
+        -1, 0, or 1.  Throw ValueError if you want to declare that the strings don't sort.
         """
-        VersionCallbacks.callbacks += [callback]
+        VersionCallback.callback[0] = callback
 
-    def apply(self, v1, v2):
-        """Call the version callbacks on v1, v2"""
+    def apply(self, v1, v2, compar):
+        """Call the version callback on v1, v2; compar is the usual version comparison function"""
 
         if v1:
             v1 = v1[:]
         if v2:
             v2 = v2[:]
         
-        for hook in VersionCallbacks.callbacks:
-            v1, v2 = hook(v1, v2)
-
-        return v1, v2
+        if VersionCallback.callback[0]:
+            return VersionCallback.callback[0](v1, v2, compar)
+        else:
+            return compar(v1, v2)
 
     def clear(self):
-        """Clear the list of version callbacks"""
-        VersionCallbacks.callbacks = []
+        """Clear the version callback"""
+        VersionCallback.callback[0] = None
 
 try:
-    type(versionCallbacks)
+    type(versionCallback)
 except NameError:
-    versionCallbacks = VersionCallbacks()
+    versionCallback = VersionCallback()
 
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
@@ -1975,7 +1975,7 @@ class Eups(object):
 
                 for productName in productNames:
                     versionNames = self.versions[db][self.flavor][productName].keys()
-                    versionNames.sort(self.version_cmp)
+                    versionNames.sort(_version_cmp)
 
                     print "  %-20s %s" % (productName, " ".join(versionNames))
             except IndexError, e:
@@ -2224,7 +2224,7 @@ match fails.
                     if vers.info.has_key(self.flavor):
                         versionNames += [(vers.version, vers.info[self.flavor])]
 
-                versionNames.sort(lambda a, b: self.version_cmp(a[0], b[0]))
+                versionNames.sort(lambda a, b: _version_cmp(a[0], b[0]))
                 versionNames.reverse() # so we'll try the latest version first
                 #
                 # Include the current version;  if it matches we'll use it
@@ -2528,7 +2528,7 @@ match fails.
         else:
             return re.search(Eups._relop_re, versionName)
 
-    def version_cmp(self, v1, v2, suffix=False):
+    def version_cmp(self, v1, v2):
         """Compare two version strings
 
     The strings are split on [._] and each component is compared, numerically
@@ -2547,109 +2547,14 @@ match fails.
     As an alternative appealing to cvs users, you can replace -EE by mEE or +FF by pFF, but in
     this case EE and FF must be integers
     """
-        def split_version(version):
-            # Split a version string of the form VVV([m-]EEE)?([p+]FFF)?
-            if not version:
-                return "", "", ""
 
-            if len(version.split("-")) > 2: # a version string such as rel-0-8-2 with more than one hyphen
-                return version, "", ""
-
-            mat = re.search(r"^([^-+]+)((-)([^-+]+))?((\+)([^-+]+))?", version)
-            vvv, eee, fff = mat.group(1), mat.group(4), mat.group(7)
-
-            if not eee and not fff:             # maybe they used VVVm# or VVVp#?
-                mat = re.search(r"(m(\d+)|p(\d+))$", version)
-                if mat:
-                    suffix, eee, fff = mat.group(1), mat.group(2), mat.group(3)
-                    vvv = re.sub(r"%s$" % suffix, "", version)
-
-            return vvv, eee, fff
-
-        v1, v2 = versionCallbacks.apply(v1, v2)
-
-        prim1, sec1, ter1 = split_version(v1)
-        prim2, sec2, ter2 = split_version(v2)
-
-        if prim1 == prim2:
-            if sec1 or sec2 or ter1 or ter2:
-                if sec1 or sec2:
-                    if (sec1 and sec2):
-                        ret = self.version_cmp(sec1, sec2, True)
-                    else:
-                        if sec1:
-                            return -1
-                        else:
-                            return 1
-
-                    if ret == 0:
-                        return self.version_cmp(ter1, ter2, True)
-                    else:
-                        return ret
-
-                return self.version_cmp(ter1, ter2, True)
-            else:
-                return 0
-
-        c1 = re.split(r"[._]", prim1)
-        c2 = re.split(r"[._]", prim2)
-        #
-        # Check that leading non-numerical parts agree
-        #
-        if not suffix:
-            prefix1, prefix2 = "", ""
-            mat = re.search(r"^([^0-9]+)", c1[0])
-            if mat:
-                prefix1 = mat.group(1)
-
-            mat = re.search(r"^([^0-9]+)", c2[0])
-            if mat:
-                prefix2 = mat.group(1)
-
-            if len(prefix1) > len(prefix2): # take shorter prefix
-                prefix = prefix2
-                if not re.search(r"^%s" % prefix, c1[0]):
-                    return +1
-            else:
-                prefix = prefix1
-                if not re.search(r"^%s" % prefix1, c2[0]):
-                    return -1
-
-            c1[0] = re.sub(r"^%s" % prefix, "", c1[0])
-            c2[0] = re.sub(r"^%s" % prefix, "", c2[0])
-
-        n1 = len(c1); n2 = len(c2)
-        if n1 < n2:
-            n = n1
-        else:
-            n = n2
-
-        for i in range(n):
-            try:                        # try to compare as integers, having stripped a common prefix
-                _c2i = None             # used in test for a successfully removing a common prefix
-
-                mat = re.search(r"^([^\d]+)\d+$", c1[i])
-                if mat:
-                    prefixi = mat.group(1)
-                    if re.search(r"^%s\d+$" % prefixi, c2[i]):
-                        _c1i = int(c1[i][len(prefixi):])
-                        _c2i = int(c2[i][len(prefixi):])
-
-                if _c2i is None:
-                    _c1i = int(c1[i])
-                    _c2i = int(c2[i])
-
-                c1[i] = _c1i
-                c2[i] = _c2i
-            except ValueError:
-                pass
-                
-            different = cmp(c1[i], c2[i])
-            if different:
-                return different
-
-        # So far, the two versions are identical.  The longer version should sort later
-        return cmp(n1, n2)
+        try:
+            return versionCallback.apply(v1, v2, _version_cmp)
+        except ValueError:
+            return None
+        except Exception, e:
+            print >> sys.stderr, "Detected error running versionCallback: %s" % e
+            return None
 
     def version_match(self, vname, expr):
         """Return vname if it matches the logical expression expr"""
@@ -2692,16 +2597,21 @@ match fails.
 
     Uses version_cmp to define sort order """
 
+        cmp = self.version_cmp(v1, v2)
+
+        if cmp is None:                 # no sort order is defined
+            return False
+
         if op == "<":
-            return self.version_cmp(v1, v2) <  0
+            return cmp <  0
         elif (op == "<="):
-            return self.version_cmp(v1, v2) <= 0
+            return cmp <= 0
         elif (op == "=="):
-            return self.version_cmp(v1, v2) == 0
+            return cmp == 0
         elif (op == ">"):
-            return self.version_cmp(v1, v2) >  0
+            return cmp >  0
         elif (op == ">="):
-            return self.version_cmp(v1, v2) >= 0
+            return cmp >= 0
         else:
             print >> sys.stderr, "Unknown operator %s used with %s, %s--- complain to RHL", (op, v1, v2)
 
@@ -3431,7 +3341,7 @@ match fails.
         #
         def sort_versions(a, b):
             if a[0] == b[0]:
-                return self.version_cmp(a[1], b[1])
+                return _version_cmp(a[1], b[1])
             else:
                 return cmp(a[0], b[0])
             
@@ -3640,6 +3550,116 @@ match fails.
         return useInfo.users(productName, versionName)
 
 _ClassEups = Eups                       # so we can say, "isinstance(Eups, _ClassEups)"
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+def _version_cmp(v1, v2, suffix=False):
+    """Here's the internal routine that version_cmp uses.
+    It's split out so that we can pass it to the callback
+    """
+
+    def split_version(version):
+        # Split a version string of the form VVV([m-]EEE)?([p+]FFF)?
+        if not version:
+            return "", "", ""
+
+        if len(version.split("-")) > 2: # a version string such as rel-0-8-2 with more than one hyphen
+            return version, "", ""
+
+        mat = re.search(r"^([^-+]+)((-)([^-+]+))?((\+)([^-+]+))?", version)
+        vvv, eee, fff = mat.group(1), mat.group(4), mat.group(7)
+
+        if not eee and not fff:             # maybe they used VVVm# or VVVp#?
+            mat = re.search(r"(m(\d+)|p(\d+))$", version)
+            if mat:
+                suffix, eee, fff = mat.group(1), mat.group(2), mat.group(3)
+                vvv = re.sub(r"%s$" % suffix, "", version)
+
+        return vvv, eee, fff
+
+    prim1, sec1, ter1 = split_version(v1)
+    prim2, sec2, ter2 = split_version(v2)
+
+    if prim1 == prim2:
+        if sec1 or sec2 or ter1 or ter2:
+            if sec1 or sec2:
+                if (sec1 and sec2):
+                    ret = _version_cmp(sec1, sec2, True)
+                else:
+                    if sec1:
+                        return -1
+                    else:
+                        return 1
+
+                if ret == 0:
+                    return _version_cmp(ter1, ter2, True)
+                else:
+                    return ret
+
+            return _version_cmp(ter1, ter2, True)
+        else:
+            return 0
+
+    c1 = re.split(r"[._]", prim1)
+    c2 = re.split(r"[._]", prim2)
+    #
+    # Check that leading non-numerical parts agree
+    #
+    if not suffix:
+        prefix1, prefix2 = "", ""
+        mat = re.search(r"^([^0-9]+)", c1[0])
+        if mat:
+            prefix1 = mat.group(1)
+
+        mat = re.search(r"^([^0-9]+)", c2[0])
+        if mat:
+            prefix2 = mat.group(1)
+
+        if len(prefix1) > len(prefix2): # take shorter prefix
+            prefix = prefix2
+            if not re.search(r"^%s" % prefix, c1[0]):
+                return +1
+        else:
+            prefix = prefix1
+            if not re.search(r"^%s" % prefix1, c2[0]):
+                return -1
+
+        c1[0] = re.sub(r"^%s" % prefix, "", c1[0])
+        c2[0] = re.sub(r"^%s" % prefix, "", c2[0])
+
+    n1 = len(c1); n2 = len(c2)
+    if n1 < n2:
+        n = n1
+    else:
+        n = n2
+
+    for i in range(n):
+        try:                        # try to compare as integers, having stripped a common prefix
+            _c2i = None             # used in test for a successfully removing a common prefix
+
+            mat = re.search(r"^([^\d]+)\d+$", c1[i])
+            if mat:
+                prefixi = mat.group(1)
+                if re.search(r"^%s\d+$" % prefixi, c2[i]):
+                    _c1i = int(c1[i][len(prefixi):])
+                    _c2i = int(c2[i][len(prefixi):])
+
+            if _c2i is None:
+                _c1i = int(c1[i])
+                _c2i = int(c2[i])
+
+            c1[i] = _c1i
+            c2[i] = _c2i
+        except ValueError:
+            pass
+
+        different = cmp(c1[i], c2[i])
+        if different:
+            return different
+
+    # So far, the two versions are identical.  The longer version should sort later
+    return cmp(n1, n2)
+
 
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 #
