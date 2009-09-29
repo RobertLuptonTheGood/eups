e16bfe926d77065891b91ab29fecd7c91f0a7f6c
Ray Plante <rplante@ncsa.uiuc.edu>
2009-09-29 22:02:44 +0000

    import Eups() ctor into eups module; move server.py into distrib module

--- python/eups/__init__.py
+++ python/eups/__init__.py
@@ -1,10 +1,11 @@
 from exceptions import *
-from tags import TagNotRecognized
-from Product import Product
+from tags       import TagNotRecognized
+from Product    import Product
+from Eups       import Eups
 
 import utils
 
-from utils import version, Quiet
-from utils import determineFlavor as flavor
+from utils      import version, Quiet
+from utils      import determineFlavor as flavor
 
 
