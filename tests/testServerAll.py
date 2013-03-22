a76efb05e30be309daee0ce0565975b127551a81
Robert Lupton the Good <rhl@astro.princeton.edu>
2013-03-22 13:04:56 -0400

    Disable tests that rely on a server that no longer exists

--- tests/testServerAll.py
+++ tests/testServerAll.py
@@ -2,10 +2,14 @@
 """
 A master script for running all tests.
 """
-import unittest
+import sys, unittest
 from testServerLocal import *
-from testServerWeb import *
-from testServerSsh import *
+
+if False:
+    from testServerWeb import *
+    from testServerSsh import *
+else:
+    print >> sys.stderr, "The remote server tests rely on a server that used to run on lsstcorp.org; skipping"
 
 if __name__ == "__main__":
     unittest.main()
