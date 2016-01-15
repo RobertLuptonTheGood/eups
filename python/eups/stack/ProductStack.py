RobertLuptonTheGood/eups

commit f86f275057753bdfb517a1ee69c78665d9694010

stack/ProductStack.py: pickle file needs to be opened in binary mode
Fixes #70

python/eups/stack/ProductStack.py
@@ -299,7 +299,7 @@ class ProductStack(object):
             self.lookup[flavor] = {}
         flavorData = self.lookup[flavor]
 
-        fd = utils.AtomicFile(file, "w")
+        fd = utils.AtomicFile(file, "wb")
         pickle.dump(flavorData, fd)
         fd.close()
         self.modtimes[file] = os.stat(file).st_mtime

commit dab052be423936b15eec842f489ddc81d2565c9e

Reverse commit f86f275 which broke eups on python 2

python/eups/stack/ProductStack.py
@@ -299,7 +299,7 @@ class ProductStack(object):
             self.lookup[flavor] = {}
         flavorData = self.lookup[flavor]
 
-        fd = utils.AtomicFile(file, "wb")
+        fd = utils.AtomicFile(file, "w")
         pickle.dump(flavorData, fd)
         fd.close()
         self.modtimes[file] = os.stat(file).st_mtime