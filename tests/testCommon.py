9c8a49ccb3e74335ff86efd294af45c6e77f1ad1
Russell Owen <rowen@uw.edu>
2016-01-16 08:41:34 -0800

    Remove unused code from testCommon.py
    tests/testCommon.py was setting EUPS_DIR to an incorrect path,
    and it turns out it was being ignored, so stop doing it.
    Also the function findFileFromRoot was not being used, so I ditched it.

--- tests/testCommon.py
+++ tests/testCommon.py
@@ -1,7 +1,6 @@
 import os, sys, unittest
 
 testEupsStack = os.path.dirname(__file__)
-os.environ["EUPS_DIR"] = os.path.dirname(testEupsStack)
 
 # clear out any products setup in the environment as these can interfere 
 # with the tests
@@ -39,30 +38,3 @@ def run(suite, exit=True):
         sys.exit(status)
     else:
         return status
-
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-def findFileFromRoot(ifile):
-    """Find file which is specified as a path relative to the toplevel directory;
-    we start in $cwd and walk up until we find the file (or throw IOError if it doesn't exist)
-
-    This is useful for running tests that may be run from <dir>/tests or <dir>"""
-    
-    if os.path.isfile(ifile):
-        return ifile
-
-    ofile = None
-    file = ifile
-    while file != "":
-        dirname, basename = os.path.split(file)
-        if ofile:
-            ofile = os.path.join(basename, ofile)
-        else:
-            ofile = basename
-
-        if os.path.isfile(ofile):
-            return ofile
-
-        file = dirname
-
-    raise IOError("Can't find %s" % ifile)
