RobertLuptonTheGood/eups

commit 6e234b40993a0dfaab68e101e115e82779862a8d

Support e.g. eups declare -t XXX -r DDD PROD VERSION
In general, if we are changing a definition and also declaring a tag,
generate a tag:XXX version with the changed attributes

python/eups/Eups.py
@@ -2554,7 +2554,7 @@ The what argument tells us what sort of state is expected (allowed values are de
                     print >> utils.stdinfo, "You asked me to redeclare %s %s%s; I'll only declare the tag" % \
                         (productName, versionName, info)
                         
-                    dodeclare = False
+                    versionName = "tag:%s" % tag # Declare a tag:XXX version with those differences
                 else:
                     raise EupsException("Redeclaring %s %s%s; specify force to proceed" %
                                         (productName, versionName, info))
@@ -2633,7 +2633,7 @@ The what argument tells us what sort of state is expected (allowed values are de
                     self.unassignTag(tag[0], productName, None, p.stackRoot(), eupsPathDir)
                 #
                 # And set it in the Proper Place
-                #                        
+                #
                 for eupsDir in eupsDirs:
                     try:
                         self.assignTag(tag[0], productName, versionName, eupsPathDir, eupsDir)

Release_Notes
@@ -1,3 +1,9 @@
+2014-04-21 rhl
+      Support e.g. eups declare -t XXX -r DDD PROD VERSION
+
+      In general, if we are changing a definition and also declaring a tag, generate
+      a tag:XXX version with the changed attributes
+
 2014-04-04 rhl
       Added unsetup{Required|Optional} to table files.
 

commit 186e117c6a129b3522297c0e258ad7355556b859

Allow redeclaring only of products with 'tag:XXX' versions
Fix a problem introduced in 6e234b40. I think the intent of that commit was
to allow re-declaration of a product's directory if it's been previously
declared with a 'tag:<tagname>' version.  This allows for convenient usage
such as:

    setup -r . -t mytest
    ... change to a different copy of the product ...
    setup -r . -t mytest

where now the second copy is the one that's declared (and declared with a
version of 'tag:mytest', and a tag 'mytest').  The way this was implemented,
however, has had an odd side-effect of allowing things such as:

    eups declare -r /stack/foo/1.2 foo 1.2 -t bar
    eups declare -r /stack/foo/1.4 foo 1.2 -t bar

which would result in the following declared products:

    foo, with version 1.2 in /stack/foo/1.2
    foo, with version tag:bar in /stack/foo/1.4

(and, more broadly, redeclaration with _any_ change would always end up
declaring a product with version 'tag:<tagname>')

The implementation has now been changed to return to the old behavior of
refusing to redeclare if there are changes in productDir, etc., unless the
version equals 'tag:<tagname>'.

python/eups/Eups.py
@@ -2568,10 +2568,25 @@ The what argument tells us what sort of state is expected (allowed values are de
                     info = " (%s)" % "; ".join(differences)
 
                 if tag:
-                    print("You asked me to redeclare %s %s%s; I'll only declare the tag" % \
-                        (productName, versionName, info), file=utils.stdinfo)
-                        
-                    versionName = "tag:%s" % tag # Declare a tag:XXX version with those differences
+                    # Allow re-declaration of a product's directory if it's been previously
+                    # declared with a 'tag:<tagname>' version. This allows for convenient
+                    # usage such as:
+                    #
+                    # setup -r . -t mytest
+                    # ... change to a different copy of the product
+                    # setup -r . -t mytest
+                    #
+                    # where now the second copy is the one that's declared.
+                    #
+                    # Otherwise, refuse to change anything other than the tag.
+                    #
+                    if versionName == "tag:%s" % tag:
+                        print("Redeclaring product %s %s in '%s' with tablefile '%s'" % (productName, versionName, productDir, tablefile),
+                            file=utils.stdinfo)
+                    else:
+                        dodeclare=False
+                        print("You asked me to redeclare %s %s%s; I'll only declare the tag" % \
+                            (productName, versionName, info), file=utils.stdinfo)
                 else:
                     raise EupsException("Redeclaring %s %s%s; specify force to proceed" %
                                         (productName, versionName, info))