a70e91868bb33ae8459ed130aaa132da101df839
Tim Jenness <tjenness@lsst.org>
2016-03-22 12:06:07 -0700

    See if eups list works
    If setups are broken then you will get a warning from eups list

--- tests/testSpaces/testSetup.sh
+++ tests/testSpaces/testSetup.sh
@@ -6,4 +6,9 @@ source "$EUPS_DIR/bin/setups.sh"
 cd "space path"
 
 setup -k -r .
+if [ $(eups list 2>&1 | grep -c Problem) != "0" ]; then
+  echo Problem detected in eups list
+  exit 1
+fi
+
 unsetup -r .
