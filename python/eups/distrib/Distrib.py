RobertLuptonTheGood/eups

commit cf23cdf61b941db142a5392dfc28cf71775f8cd0

Modernize sort in eups distrib
Convert a bare cmp function into a key sort function. This is
required for python 3.

python/eups/distrib/Distrib.py
@@ -449,10 +449,10 @@ class Distrib(object):
         # The first thing to do is to ensure that more deeply nested products are listed first as we need to
         # build them first when installing
         #
-        def byDepth(a, b):
+        def byDepth(a):
             """Sort by recursion depth"""
-            return cmp(b[2], a[2])
-        dependencies.sort(byDepth)
+            return a[2]
+        dependencies.sort(key=byDepth)
 
         for (dprod, dopt, recursionDepth) in dependencies:
             dproductName = dprod.name

commit 212399718e4643858a076c6a781005fca8bb75a1

Fix depth sorting of dependencies
Broken in cf23cdf61b941db142a5392dfc28cf71775f8cd0 when the cmp(b,a)
was replaced with a key sort on integer rather than a sort on the negative
integer.

python/eups/distrib/Distrib.py
@@ -451,7 +451,7 @@ class Distrib(object):
         #
         def byDepth(a):
             """Sort by recursion depth"""
-            return a[2]
+            return -a[2]
         dependencies.sort(key=byDepth)
 
         for (dprod, dopt, recursionDepth) in dependencies: