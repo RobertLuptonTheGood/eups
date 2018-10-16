9c736bc2e31ad9aa424373cbe4dee398a608e978
Tim Jenness <tim.jenness@gmail.com>
2018-10-16 13:15:54 -0700

    Install csh in yaml matrix
    No longer install csh via environment variable in script.sh.
    This does not seem to be a reliable approach in Travis. It is
    much better to tell travis to install csh explicitly.
    
    Retain the env var since that is helpful to see in the Travis
    log.

--- ci/script.sh
+++ ci/script.sh
@@ -3,11 +3,6 @@
 # A script that installs and tests EUPS on TravisCI
 #
 
-# Should we test with csh installed?
-if [[ $CI_WITH_CSH == 1 && "$TRAVIS_OS_NAME" == linux ]]; then
-	sudo apt-get install -y csh
-fi
-
 # configure
 PREFIX=$(mktemp -d -t "eupstest XXXXXX")
 ./configure --prefix="$PREFIX" --with-python=$(which python)
